f584b006b4a7818793cea489a85b5ae8320a924d
Austin Nobregas <austinnobregas@gmail.com>
2023-05-05 21:13:14 -0400

    outline and redesighn of previous code

---
+++ models/comment.js
@@ -0,0 +1,30 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Comment extends Model { }
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    Comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // define columns
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'Comment',
+  }
+);
+
+module.exports = Comment;
